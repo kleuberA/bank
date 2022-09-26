6552a2f5acd17f1a3d0f009ebbed671d51f3a8bc
Kleuber Silva <kleuber20@hotmail.com>
2022-09-26 16:22:20 -0300

    Adicionar arquivos de projeto.

---
+++ bank/ContaCorrente.cs
@@ -0,0 +1,126 @@
+﻿using bank.Titular;
+
+namespace bank
+{
+    public class ContaCorrente
+    {
+
+        public Cliente Titular { get; set; }
+
+        private string _conta;
+        public string Conta 
+        {
+            get
+            {
+                return _conta;
+            }
+            set
+            {
+                if(value == null)
+                {
+                    return;
+                }
+                else
+                {
+                    _conta = value;
+                }
+            }
+        }
+
+        private int _numero_agencia;
+        public int Numero_agencia 
+        {
+            get
+            {
+                return _numero_agencia;
+            }
+            set
+            {
+                if(value <= 0)
+                {
+
+                }
+                else
+                {
+                    _numero_agencia = value;
+                }
+            }
+        }
+        public string Nome_agencia { get; set; }
+
+        private double saldo;
+
+        public bool Sacar(double valor)
+        {
+            if(saldo < valor)
+            {
+                return false;
+            }
+            if(valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo = saldo + valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+        }
+
+        //public void SetSaldo(double valor)
+        //{
+        //    if(valor < 0) { return; }
+        //    saldo += valor;
+        //}
+
+        //public double GetSaldo()
+        //{
+        //    return saldo;
+        //}
+
+        public double Saldo
+        {
+            get { return saldo; }
+            set
+            {
+                if(value < 0)
+                {
+                    return;
+                }
+                saldo = value;
+            }
+        }
+
+           public ContaCorrente( string conta, int numero_agencia)
+        {
+            Conta = conta;
+            Numero_agencia = numero_agencia;
+            TotalDeContasCriadas += 1;
+        }
+
+        public static int TotalDeContasCriadas { get; set; }
+    }
+}
---
+++ bank/Program.cs
@@ -0,0 +1,30 @@
+﻿using bank;
+using bank.Titular;
+
+ContaCorrente conta1 = new ContaCorrente("10123-X", 23);
+
+conta1.Nome_agencia = "Agencia central";
+
+
+Cliente cliente = new Cliente();
+cliente.Nome = "Kleuber Aguiar";
+cliente.Cpf = "15040585000";
+cliente.Profissao = "Programador CSharp";
+
+
+Console.WriteLine("Titular: " + cliente.Nome);
+Console.WriteLine("Conta: " + conta1.Conta);
+Console.WriteLine("Numero agencia: " + conta1.Numero_agencia);
+Console.WriteLine("Nome agencia: " + conta1.Nome_agencia);
+
+Cliente kleuber = new Cliente();
+
+ContaCorrente conta4 = new ContaCorrente("10623-X", 235);
+conta4.Saldo = 200;
+conta4.Titular = kleuber;
+
+Console.WriteLine("Saldo: " + conta4.Saldo);
+Console.WriteLine("Contas Criadas: " + ContaCorrente.TotalDeContasCriadas);
+
+
+Console.ReadKey();
